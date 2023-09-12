6528f60e83043e812e8c53b96034b6500e2b194d
Henrique Barucco <henrique_barucco@hotmail.com>
2023-09-12 14:30:00 -0300

    feat(skill-icon.tsx): adiciona propriedade 'img' ao componente SkillIcon feat(skills.tsx): atualiza as chamadas para o componente SkillIcon com a propriedade 'img'
    Agora o componente SkillIcon recebe uma propriedade 'img' que permite especificar a URL da imagem a ser exibida. Isso torna o componente mais flexível, pois agora pode exibir ícones de habilidades diferentes, dependendo da propriedade 'img' fornecida. As chamadas para o componente SkillIcon em skills.tsx foram atualizadas para incluir a propriedade 'img' correspondente a cada habilidade.

--- src/components/skill-icon.tsx
+++ src/components/skill-icon.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 
-export default function SkillIcon({language}: {language: string}) {
+export default function SkillIcon({language, img}: {language: string, img: string}) {
     return (
         <div className='flex flex-row space-x-2 align-middle'>
-            <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt='java' width={24} height={24} />
+            <Image src={img} alt='java' width={24} height={24} />
             <p className='text-sm font-bold text-center self-center'>{language}</p>
         </div>
     );
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -7,11 +7,11 @@ export default function Skills() {
                 <p className='text-1xl font-bold text-end'>HABILIDADES</p>
             </div>
             <div className='flex-1 space-y-2'>
-                <SkillIcon language='Java'/>
-                <SkillIcon language='Javascript'/>
-                <SkillIcon language='Typescript'/>
-                <SkillIcon language='Golang'/>
-                <SkillIcon language='Python'/>
+                <SkillIcon language='Java' img='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'/>
+                <SkillIcon language='Javascript' img='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'/>
+                <SkillIcon language='Typescript' img='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg'/>
+                <SkillIcon language='Golang' img='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg'/>
+                <SkillIcon language='Python' img='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'/>
             </div>
         </div>
     );
