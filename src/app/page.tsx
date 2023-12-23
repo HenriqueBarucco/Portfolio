9755657a88059b45a1621cf9dea76ba475baf7f9
Henrique Barucco <henrique_barucco@hotmail.com>
2023-12-22 21:08:57 -0300

    feat: improve experiences list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default async function Home() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
                 </svg>
                 <div className='flex bg-base-200 w-full justify-center items-center py-4'>
-                    <div className='flex flex-col w-2/5 justify-center space-y-10' id='sobre'>
+                    <div className='flex flex-col w-3/5 justify-center space-y-10' id='sobre'>
                         <AboutMe about={user.aboutMe}/>
                         <Skills skills={user.skills}/>
-                        <Experiences/>
+                        <Experiences experiences={user.experiences} />
                         <Education educations={user.educations}/>
                     </div>
                 </div>
@@ -27,8 +27,8 @@ export default async function Home() {
 
 const user = {
     name: 'Henrique Barucco',
-    title: 'Desenvolvedor Fullstack',
-    aboutMe: 'Sou um Desenvolvedor Full Stack apaixonado por tecnologia com uma habilidade comprovada em trabalhar em equipe, resolução de problemas e comunicação eficaz. Sempre buscando oportunidades para aprimorar minhas habilidades e oferecer soluções inovadoras para atender às expectativas dos clientes. Com uma mentalidade de crescimento constante, eu estou sempre em busca de novos desafios e oportunidades para expandir meus conhecimentos e habilidades.',
+    title: 'Desenvolvedor Backend',
+    aboutMe: 'Sou um Desenvolvedor Backend apaixonado por tecnologia com uma habilidade comprovada em trabalhar em equipe, resolução de problemas e comunicação eficaz. Sempre buscando oportunidades para aprimorar minhas habilidades e oferecer soluções inovadoras para atender às expectativas dos clientes. Com uma mentalidade de crescimento constante, eu estou sempre em busca de novos desafios e oportunidades para expandir meus conhecimentos e habilidades.',
     skills: [
         {
             name: 'Java',
@@ -85,6 +85,47 @@ const user = {
         {
             name: 'Jenkins',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg'
+        }
+    ],
+    experiences: [
+        {
+            company: 'LuizaLabs',
+            role: 'Desenvolvedor Backend Pleno',
+            startDate: 'Dezembro 2023',
+            endDate: null,
+            description: [
+                'Java/Kotlin com Spring Boot.',
+                'Microsserviços.',
+                'Kubernetes.',
+                'Node.js com Nest.js.',
+                'Golang.'
+            ]
+        },
+        {
+            company: 'GVD Soluções Inteligentes',
+            role: 'Desenvolvedor Backend Júnior',
+            startDate: 'Agosto 2022',
+            endDate: 'Dezembro 2023',
+            description: [
+                'Desenvolvimento de APIs em Java com o framework Spring (Spring Boot, Spring Security e Spring Cloud).',
+                'Integração contínua e entrega contínua (CI/CD) com ferramentas como GitLab.',
+                'Utilização de Docker para criação de ambientes de desenvolvimento e implantação.',
+                'Desenvolvimento de aplicações em Node.js.',
+                'Criação de sistemas de conexão com o WhatsApp para disparo de mensagens.',
+                'Trabalho em equipes multidisciplinares.'
+            ]
+        },
+        {
+            company: 'Instituto Espirita Paulo de Tarso',
+            role: 'Iniciação Científica - Fullstack',
+            startDate: 'Outubro 2022',
+            endDate: 'Novembro 2023',
+            description: [
+                'Desenvolvimento de plataforma para gerenciamento de Aulas.',
+                'API desenvolvida com o framework Spring.',
+                'Frontend desenvolvido com o framework Next JS (React).',
+                'Ambientes de desenvolvimento e produção utilizando Docker.'
+            ]
         }
     ],
     educations: [
--- src/components/about/experiences.tsx
+++ src/components/about/experiences.tsx
@@ -1,42 +1,30 @@
-export default function Experiences() {
+export default function Experiences({experiences}: {experiences: any[]}) {
     return (
         <div className='flex flex-row'>
             <div className='w-[160px] pr-4'>
                 <p className='text-1xl font-bold text-end'>EXPERIÊNCIAS</p>
             </div>
             <div className='flex-1 space-y-4'>
-                <div className='flex flex-col'>
-                    <div className='flex flex-row w-full'>
-                        <p className='font-bold text-start'>GVD Soluções Inteligentes</p>
-                        <p className='grow text-end'>Agosto 2022 - Atual</p>
-                    </div>
-                    <div>
-                        <p className='text-start'>Desenvolvedor Backend</p>
-                        <ul className="list-disc list-inside text-xs mt-2">
-                            <li className="mb-2">Desenvolvimento de APIs em Java com o framework Spring (Spring Boot, Spring Security e Spring Cloud).</li>
-                            <li className="mb-2">Integração contínua e entrega contínua (CI/CD) com ferramentas como GitLab.</li>
-                            <li className="mb-2">Utilização de Docker para criação de ambientes de desenvolvimento e implantação.</li>
-                            <li className="mb-2">Desenvolvimento de aplicações em Node.js.</li>
-                            <li className="mb-2">Criação de sistemas de conexão com o WhatsApp para disparo de mensagens.</li>
-                            <li className="mb-2">Trabalho em equipes multidisciplinares.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='flex flex-col'>
-                    <div className='flex flex-row w-full'>
-                        <p className='font-bold text-start'>Instituto Espirita Paulo de Tarso</p>
-                        <p className='grow text-end'>Outubro 2022 - Atual</p>
-                    </div>
-                    <div>
-                        <p className='text-start'>Iniciação Científica - Fullstack</p>
-                        <ul className="list-disc list-inside text-xs mt-2">
-                            <li className="mb-2">Desenvolvimento de plataforma para gerenciamento de Aulas.</li>
-                            <li className="mb-2">API desenvolvida com o framework Spring.</li>
-                            <li className="mb-2">Frontend desenvolvido com o framework Next JS (React).</li>
-                            <li className="mb-2">Ambientes de desenvolvimento e produção utilizando Docker.</li>
-                        </ul>
-                    </div>
-                </div>
+                {
+                    experiences.map((experience, index) => (
+                        <div className='flex flex-col' key={index}>
+                            <div className='flex flex-row w-full'>
+                                <p className='font-bold text-start'>{experience.company}</p>
+                                <p className='grow text-end'>{experience.startDate} - {experience.endDate || 'Atual'}</p>
+                            </div>
+                            <div>
+                                <p className='text-start'>{experience.role}</p>
+                                <ul className="list-disc list-inside text-xs mt-2">
+                                    {
+                                        experience.description.map((description: string, index: number) => (
+                                            <li className="mb-2" key={index}>{description}</li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
